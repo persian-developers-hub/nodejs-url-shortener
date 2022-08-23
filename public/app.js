33469e739426b3e3f39747e43dbe1498223379d6
ehsanghaffarii <ghafari.5000@gmail.com>
2022-08-23 11:05:53 +0430

    first version

---
+++ public/app.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    url: "",
+    slug: "",
+    error: "",
+    formVisible: true,
+    created: null,
+  },
+  methods: {
+    async createUrl() {
+      this.error = "";
+      const response = await fetch("/url", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          url: this.url,
+          slug: this.slug || undefined,
+        }),
+      });
+      if (response.ok) {
+        const result = await response.json();
+        this.formVisible = false;
+        // TODO: Change this on production
+        this.created = `http//localhost:1337/${result.slug}`;
+      } else if (response.status === 429) {
+        this.error =
+          "You are sending too many requests. Try again in 30 seconds.";
+      } else {
+        const result = await response.json();
+        this.error = result.message;
+      }
+    },
+  },
+});
